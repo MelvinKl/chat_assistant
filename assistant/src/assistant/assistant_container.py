f636498ed74186c9f5a2b8edc4dd087adb169646
Melvin Klein <70433111+MelvinKl@users.noreply.github.com>
2025-07-19 07:16:49 +0200

    chore: add errorhandling during mcp tool loading (#65)

--- assistant/src/assistant/assistant_container.py
+++ assistant/src/assistant/assistant_container.py
@@ -1,9 +1,11 @@
 import asyncio
+import logging
 
 import inject
 import nest_asyncio
 from inject import Binder
 from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.tools import BaseTool
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
@@ -20,19 +22,14 @@ from assistant.impl.settings.prompt_settings import PromptSettings
 # Apply the patch to allow nested event loops
 nest_asyncio.apply()
 
+logger = logging.getLogger(__name__)
 
-def _di_config(binder: Binder):
-    settings_openai = OpenAISetttings()
-    settings_prompt = PromptSettings()
-    settings_mcp = load_mcp_settings_from_json()
 
-    llm = ChatOpenAI(
-        model=settings_openai.model,
-        base_url=settings_openai.base_url,
-        api_key=settings_openai.api_key,
-    )
-    server_dict = {}
+def _get_mcp_tools(settings_mcp: MCPSettings) -> list[BaseTool]:
+    tools = []
+
     for server_definition in settings_mcp.servers:
+        server_dict = {}
         if server_definition.transport == "stdio":
             server_dict[server_definition.name] = {
                 "command": server_definition.command,
@@ -46,9 +43,26 @@ def _di_config(binder: Binder):
             }
             if server_definition.headers:
                 server_dict[server_definition.name]["headers"] = server_definition.headers
+        mcp_client = MultiServerMCPClient(server_dict)
+        try:
+            server_tools = asyncio.run(mcp_client.get_tools())
+            tools += server_tools
+        except Exception as e:
+            logger.error("Could not load MCP Tools from server %s\t%s " % (server_definition.name, e))
+    return tools
 
-    mcp_client = MultiServerMCPClient(server_dict)
-    tools = asyncio.run(mcp_client.get_tools())
+
+def _di_config(binder: Binder):
+    settings_openai = OpenAISetttings()
+    settings_prompt = PromptSettings()
+    settings_mcp = load_mcp_settings_from_json()
+
+    llm = ChatOpenAI(
+        model=settings_openai.model,
+        base_url=settings_openai.base_url,
+        api_key=settings_openai.api_key,
+    )
+    tools = _get_mcp_tools(settings_mcp)
     mcp_agent = create_react_agent(llm, tools)
 
     binder.bind(
